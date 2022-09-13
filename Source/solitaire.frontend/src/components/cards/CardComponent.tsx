10cb398be7e2299152ebc40dad40856771c0795e
bibletoon <alex8.ivanov@gmail.com>
2022-09-13 22:41:24 +0300

    Refactor: extract common styles

--- Source/solitaire.frontend/src/components/cards/CardComponent.tsx
+++ Source/solitaire.frontend/src/components/cards/CardComponent.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './CardComponent.module.css';
 import Card from "../../models/Card";
+import Suit from "../../models/Suit";
 
 
 interface CardComponentProps {
@@ -8,8 +9,9 @@ interface CardComponentProps {
 }
 
 const CardComponent = ({card} : CardComponentProps) => {
+    const color = card.suit == Suit.Heart || card.suit == Suit.Diamond ? styles.red : styles.black;
     return (
-        <div draggable={true} className={styles.card}>{card.toChar()}</div>
+        <div draggable={true} className={[styles.card, color].join(" ")}>{card.toChar()}</div>
     );
 };
 
