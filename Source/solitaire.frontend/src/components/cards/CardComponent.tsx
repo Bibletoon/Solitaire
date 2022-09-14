c64b2f6f3d9123b1bbe22a317c5403b7e7192681
bibletoon <alex8.ivanov@gmail.com>
2022-09-14 13:12:04 +0300

    Fix: hidden card color

--- Source/solitaire.frontend/src/components/cards/CardComponent.tsx
+++ Source/solitaire.frontend/src/components/cards/CardComponent.tsx
@@ -10,7 +10,7 @@ type CardComponentProps = {
 }
 
 const CardComponent : FC<CardComponentProps> = ({card, hidden = false} : CardComponentProps) => {
-    const color = card.suit === Suit.Heart || card.suit === Suit.Diamond ? styles.red : styles.black;
+    const color = hidden ? styles.red : card.suit === Suit.Heart || card.suit === Suit.Diamond ? styles.red : styles.black;
     return (
         <div draggable={true} className={[styles.card, color].join(" ")}>{hidden ? '\u{1F0A0}' : card.toChar()}</div>
     );
