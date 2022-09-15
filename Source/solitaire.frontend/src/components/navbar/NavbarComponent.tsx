65efb523795e715c0ab1f5b1901564a465274e0b
bibletoon <alex8.ivanov@gmail.com>
2022-09-15 23:20:24 +0300

    Refactor: restyle navbar

--- Source/solitaire.frontend/src/components/navbar/NavbarComponent.tsx
+++ Source/solitaire.frontend/src/components/navbar/NavbarComponent.tsx
@@ -1,15 +1,17 @@
 import React, {FC} from 'react';
 import styles from "./Navbar.module.css"
+import {AccountCircle} from "@mui/icons-material";
+
 
 const NavbarComponent : FC = () => {
     return (
         <nav className={styles.navbar}>
             <a className={styles.navbar__brand}>
-                <img src="https://cdn-icons-png.flaticon.com/512/1687/1687631.png" alt="" className={styles.navbar__icon}/>
+                <img src="https://cdn-icons-png.flaticon.com/512/8037/8037108.png" alt="" className={styles.navbar__icon}/>
                 <span className={styles.navbar__title}>Solitaire</span>
             </a>
             <a className={styles.navbar__profile}>
-                <img src="https://cdn-icons-png.flaticon.com/512/1250/1250689.png" alt="" className={styles.navbar__avatar}/>
+                <AccountCircle fontSize="large"/>
                 <span>Bibletoon</span>
             </a>
         </nav>
