d66e50b839238d8e282c6da6a9908709009383f9
bibletoon <alex8.ivanov@gmail.com>
2022-09-19 18:17:36 +0300

    Feat: use game models in UI components

--- Source/solitaire.frontend/src/components/board/BoardComponent.tsx
+++ Source/solitaire.frontend/src/components/board/BoardComponent.tsx
@@ -5,37 +5,35 @@ import Card from "../../models/Card";
 import Suit from "../../models/Suit";
 import CardValue from "../../models/CardValue";
 import styles from "./BoardComponent.module.css"
+import SolitaireGame from "../../models/SolitaireGame";
+import ClassicDeckCreator from "../../models/deckInitializer/ClassicDeckCreator";
+import {shuffle} from "../../models/DeckUtils";
+import DeckComponent from "../cards/DeckComponent";
 
 const BoardComponent : FC = () => {
+    const deckCreator = new ClassicDeckCreator();
+    const game = new SolitaireGame(shuffle(deckCreator.create()))
+
     return (
         <div className={styles.board}>
             <div className={[styles.board__row, "row"].join(" ")}>
                 <div className={[styles.board__deck, "row"].join(" ")}>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
+                    <DeckComponent deck={game.deck.hidden} />
+                    <DeckComponent deck={game.deck.shown} hidden={false}/>
                 </div>
                 <div className={[styles.board__foundations, "row"].join(" ")}>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                    <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
+                    <DeckComponent deck={game.foundations[0]} hidden={false}/>
+                    <DeckComponent deck={game.foundations[1]} hidden={false}/>
+                    <DeckComponent deck={game.foundations[2]} hidden={false}/>
+                    <DeckComponent deck={game.foundations[3]} hidden={false}/>
                 </div>
             </div>
             <div className={[styles.board__layout, "row"].join(" ")}>
-                <CardStackComponent cards={
-                    [
-                        new Card(Suit.Heart, CardValue.ace),
-                        new Card(Suit.Heart, CardValue.ace),
-                        new Card(Suit.Heart, CardValue.ace),
-                        new Card(Suit.Heart, CardValue.ace),
-                        new Card(Suit.Heart, CardValue.ace)
-                    ]} />
-                <CardStackComponent cards={[]}/>
-                <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
-                <CardComponent card={new Card(Suit.Heart, CardValue.ace)} hidden={false}/>
+                {
+                    game.layout.map(f =>
+                        <CardStackComponent cards={f}/>
+                    )
+                }
             </div>
         </div>
     );
