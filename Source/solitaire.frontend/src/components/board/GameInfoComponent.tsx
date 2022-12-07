ffbed24e9ca66c2b5207d31acdcb36c551f5109c
bibletoon <alex8.ivanov@gmail.com>
2022-12-07 23:18:18 +0300

    Feat: add timer

--- Source/solitaire.frontend/src/components/board/GameInfoComponent.tsx
+++ Source/solitaire.frontend/src/components/board/GameInfoComponent.tsx
@@ -3,16 +3,19 @@ import styles from "./GameInfoComponent.module.css";
 import {Button} from "@mui/material";
 
 type GameInfoComponentProps = {
+    seconds : number,
+    minutes : number,
+    hours : number,
     movesCount : number,
     newGame : () => void,
     restartGame : () => void
 }
 
-const GameInfoComponent : FC<GameInfoComponentProps> = ({movesCount, newGame, restartGame}) => {
+const GameInfoComponent : FC<GameInfoComponentProps> = ({seconds, minutes, hours, movesCount, newGame, restartGame}) => {
     return (
         <div className={styles.gameInfo}>
             <div className={styles.gameInfo__stats}>
-                <div className={styles.gameInfo__item}>Время: 4:51</div>
+                <div className={styles.gameInfo__item}>Время: {hours}:{minutes}:{seconds}</div>
                 <div className={styles.gameInfo__item}>Ходы: {movesCount}</div>
             </div>
             <div className={styles.gameInfo__buttons}>
--- Source/solitaire.frontend/src/hooks/UseGame.ts
+++ Source/solitaire.frontend/src/hooks/UseGame.ts
@@ -4,6 +4,7 @@ import {shuffle} from "../models/DeckUtils";
 import CardPosition from "../models/CardPosition";
 import PlacePosition from "../models/PlacePosition";
 import ClassicDeckCreator from "../models/deckInitializer/ClassicDeckCreator";
+import {useStopwatch} from "react-timer-hook";
 
 const useGame = () => {
     const deckCreator = new ClassicDeckCreator();
@@ -11,8 +12,11 @@ const useGame = () => {
     let [startDeck, setStartDeck] = useState(deck);
     const [game, setGame] = useState(SolitaireGame.create([...startDeck]));
     const [movesCount, setMovesCount] = useState(0);
+    const {seconds, minutes, hours, start, pause, reset} = useStopwatch({autoStart: false});
 
     const moveCard = (cardPosition : CardPosition, placePosition : PlacePosition) : void => {
+        if (movesCount == 0)
+            start();
         game.moveCard(cardPosition, placePosition);
         setMovesCount(movesCount + 1);
         setGame(new SolitaireGame(game.deck, game.foundations, game.layout));
@@ -21,6 +25,8 @@ const useGame = () => {
     const canMoveCard = (cardPosition : CardPosition, placePosition : PlacePosition) => game.canMoveCard(cardPosition, placePosition);
 
     const showDeckCard = () : void => {
+        if (movesCount == 0)
+            start();
         const card = game.deck.hidden.pop();
         if (card === undefined) {
             game.deck.hidden = game.deck.shown.reverse();
@@ -35,6 +41,7 @@ const useGame = () => {
     const restartGame = () : void => {
         setMovesCount(0);
         setGame(SolitaireGame.create([...startDeck]));
+        reset(undefined, false);
     }
 
     const newGame = () : void => {
@@ -42,9 +49,10 @@ const useGame = () => {
         setStartDeck(deck);
         setMovesCount(0);
         setGame(SolitaireGame.create([...deck]));
+        reset(undefined, false);
     }
 
-    return {game, movesCount, canMoveCard, moveCard, showDeckCard, newGame, restartGame}
+    return {game, movesCount, seconds, minutes, hours, canMoveCard, moveCard, showDeckCard, newGame, restartGame}
 }
 
 export default useGame;
